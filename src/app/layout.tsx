7a723a133d3abc7935291766651a66cee764c022
Rujuu <n.ujiie2118@gmail.com>
2026-02-08 02:29:16 +0900

    :zap: GA追加,favicon追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { GoogleAnalytics } from "@/components/atoms/GoogleAnalytics";
 import "./globals.css";
 
 const inter = Inter({
@@ -8,8 +9,29 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
+  metadataBase: new URL(
+    process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000",
+  ),
   title: "Portfolio | Developer",
   description: "ものづくりを通して、できるだけ多くの人に良い影響を与えたい。",
+  icons: {
+    icon: "/favicon.ico",
+  },
+  openGraph: {
+    title: "Portfolio | Developer",
+    description: "ものづくりを通して、できるだけ多くの人に良い影響を与えたい。",
+    url: process.env.NEXT_PUBLIC_SITE_URL,
+    siteName: "Portfolio | Developer",
+    images: [{ url: "/default.png" }],
+    locale: "ja_JP",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Portfolio | Developer",
+    description: "ものづくりを通して、できるだけ多くの人に良い影響を与えたい。",
+    images: ["/default.png"],
+  },
 };
 
 export default function RootLayout({
@@ -25,7 +47,13 @@ export default function RootLayout({
           href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css"
         />
       </head>
-      <body className={`${inter.className} antialiased`} suppressHydrationWarning>{children}</body>
+      <body
+        className={`${inter.className} antialiased`}
+        suppressHydrationWarning
+      >
+        <GoogleAnalytics />
+        {children}
+      </body>
     </html>
   );
 }
